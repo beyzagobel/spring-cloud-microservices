91097cf9e404aa6dc3adee1205de94e8c03b9437
beyza <byzgbll@gmail.com>
2022-11-27 22:58:00 +0300

    init

---
+++ service-parent/src/test/java/com/beyzagobel/serviceparent/ServiceParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyzagobel.serviceparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
